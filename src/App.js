ed321e96e3f572fe12347e35b4a6ed660a9076f9
Luke Russell <thelukerussel21@gmail.com>
2020-02-03 14:45:48 -0500

    removed render

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import {render} from '@testing-library/react';
 
-// class App extends Component {
-//   render() {
-    // return(
-    //   <div className="App">
-    //     <h1>Hey what's poppin</h1>
-    //   </div>
-    // );
-    
-    // Or
-
-//     return React.createElement('div', {className:'App'}, React.createElement('h1', null, `Hey what's poppin`));
-
-    // return React.createElement('div', {className: 'App'}, React.createElement('button', null, `likes`));
-
-    // export default App;
-
-  class LikeButton extends Component {
+  class App extends Component {
 
     state = {
-      number: '0',
+      number: 0,
       likes: 'Likes'
     }
     
-    
-    increaseLikes = () => {
-      console.log('you clicked button')
+    addLikes = () => {
       this.setState = ({
-        number: this.state.number + 1,
+        number: this.state.number + 1
       })
     }
 
-
     render() {
-      console.log(this.state.number)
     return(
-      <div className="Like">
-        <button onClick={this.increaseLikes} className="button">{this.state.number} {this.state.likes}</button>
-
+      <div className="App">
+        <button onClick={this.addLikes} className="button">{this.state.number} {this.state.likes}</button>
       </div>
     )
-  };
+  }
 }
 
-export default LikeButton;
+export default App;
   
 
