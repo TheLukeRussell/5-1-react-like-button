e1ad979c12caafeb070d0121b62df1bb2c6527af
Luke Russell <thelukerussel21@gmail.com>
2020-02-03 14:21:04 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {render} from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends Component {
+//   render() {
+    // return(
+    //   <div className="App">
+    //     <h1>Hey what's poppin</h1>
+    //   </div>
+    // );
+    
+    // Or
+
+//     return React.createElement('div', {className:'App'}, React.createElement('h1', null, `Hey what's poppin`));
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('button', null, `likes`));
+
+    // export default App;
+
+  class LikeButton extends Component {
+
+    state = {
+      number: '0',
+      likes: 'Likes'
+    }
+    
+    
+    increaseLikes = () => {
+      console.log('you clicked button')
+      this.setState = ({
+        number: this.state.number + 1,
+      })
+    }
+
+
+    render() {
+      console.log(this.state.number)
+    return(
+      <div className="Like">
+        <button onClick={this.increaseLikes} className="button">{this.state.number} {this.state.likes}</button>
+
+      </div>
+    )
+  };
 }
 
-export default App;
+export default LikeButton;
+  
+
